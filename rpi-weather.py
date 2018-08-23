79490b497316c2ae6b87917a052ea3b5300476fc
erickstm87 <erickstm87@github.com>
2018-08-22 21:39:42 -0600

    start

--- rpi-weather.py
+++ rpi-weather.py
@@ -15,20 +15,15 @@ post_data = {
     'author': 'Scott'
 }
 result = posts.insert_one(post_data)
-print('One post: {0}'.format(result.inserted_id))
-
+#print('One post: {0}'.format(result))
+found_post = posts.find_one({'author': 'Scott'})
+print('\n', found_post)
 
 r = requests.get('http://api.openweathermap.org/data/2.5/weather?lat=39.7521448&lon=-105.0233895&APPID=977c4567f54065e97d732e98b76e6180&units=imperial')
 response = r.json()
 print(response)
 temp = float(response['main']['temp'])
 print('the weather is imperial ', temp)
-
-# c = conn.cursor()
-# c.execute('''CREATE TABLE weather(outside_weather, inside_weather, time)''')
-# c.execute("Insert weather (25,28,7)")
-# conn.commit()
-# conn.close()
 
 # print(outside_weather)
 # plt.plot(outside_weather)
