813ea05494c80759d501909364b31ce5fce317d4
erickstm87 <erickstm87@github.com>
2018-08-22 21:18:25 -0600

    db work

--- rpi-weather.py
+++ rpi-weather.py
@@ -1,24 +1,42 @@
 import requests
+from pymongo import MongoClient
+import sqlite3
 import matplotlib.pyplot as plt
 
-outside_weather, inside_weather = [], []
+client = MongoClient()
+# conn = sqlite3.connect('temps.db')
+
+client = MongoClient('localhost', 27017)
+db = client.pymongo_test
+posts = db.posts
+post_data = {
+    'title': 'Python and MongoDB',
+    'content': 'PyMongo is fun, you guys',
+    'author': 'Scott'
+}
+result = posts.insert_one(post_data)
+print('One post: {0}'.format(result.inserted_id))
+
+
 r = requests.get('http://api.openweathermap.org/data/2.5/weather?lat=39.7521448&lon=-105.0233895&APPID=977c4567f54065e97d732e98b76e6180&units=imperial')
 response = r.json()
 print(response)
 temp = float(response['main']['temp'])
 print('the weather is imperial ', temp)
 
-for i in range(11):
-    temp += 1
-    outside_weather.append(temp)
+# c = conn.cursor()
+# c.execute('''CREATE TABLE weather(outside_weather, inside_weather, time)''')
+# c.execute("Insert weather (25,28,7)")
+# conn.commit()
+# conn.close()
 
-print(outside_weather)
-plt.plot(outside_weather)
+# print(outside_weather)
+# plt.plot(outside_weather)
 
-plt.title('Temp In and Out of Place: ', fontsize=24)
-plt.xlabel('Time', fontsize=14)
-plt.ylabel('Temp Farenheit', fontsize=14)
+# plt.title('Temp In and Out of Place: ', fontsize=24)
+# plt.xlabel('Time', fontsize=14)
+# plt.ylabel('Temp Farenheit', fontsize=14)
 
-plt.show()
+# plt.show()
 
 # outside_weather.append()
