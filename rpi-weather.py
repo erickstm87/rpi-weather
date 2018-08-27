a1a4077c257499e343352fc4b33eb5815af7370a
Thomas Erickson <terickson@spotx.tv>
2018-08-27 10:48:30 -0600

    used pygal instead now perfect

--- rpi-weather.py
+++ rpi-weather.py
@@ -1,6 +1,7 @@
 import requests
 from pymongo import MongoClient
 import matplotlib.pyplot as plt
+import pygal
 from datetime import datetime, timedelta
 import pprint
 from pytz import timezone
@@ -16,8 +17,8 @@ print('here is the time: ', datetime.now())
 r = requests.get('http://api.openweathermap.org/data/2.5/weather?lat=39.7521448&lon=-105.0233895&APPID=977c4567f54065e97d732e98b76e6180&units=imperial')
 response = r.json()
 pprint.pprint(response)
-outside_temp = str(float(response['main']['temp']))
-print('the weather is imperial ', outside_temp)
+outside_temp = float(response['main']['temp'])
+# print('the weather is imperial ', outside_temp)
 hourly = datetime.now().hour
 minutely = datetime.now().minute
 current_time = str(hourly) + ':' + str(minutely)
@@ -25,7 +26,7 @@ current_time = str(hourly) + ':' + str(minutely)
 posts = db.posts
 weather_data = {
     'title': 'Outside Weather',
-    'temp': str(outside_temp),
+    'temp': outside_temp,
     'date': current_time
 }
 result = posts.insert_one(weather_data)
@@ -34,14 +35,16 @@ found_post = posts.find_one({'title': 'Outside Weather'})
 outside_weather, the_time = [], []
 for post in posts.find():
     outside_weather.append(post['temp'])
-    #the_time.append(post['date'])
+    the_time.append(post['date'])
 
-plt.plot(the_time, outside_weather)
+print(outside_weather)
+print(the_time)
 
-plt.title('Temp In and Out of Place with ' + response['weather'][0]['description'].title() + ' Conditions: ', fontsize=24)
-plt.xlabel('Time', fontsize=14)
-plt.ylabel('Temp Farenheit', fontsize=14)
+line_chart = pygal.Line()
+line_chart.title = ('Temp In and Out of Place with ' + response['weather'][0]['description'].title() + ' Conditions: ')
+line_chart.x_labels = map(str, the_time)
+line_chart.add('Outside', outside_weather)
+line_chart.render_to_file('weather.svg')
 
-plt.show()
-
-# outside_weather.append()
+# for post in posts.find():
+#     posts.remove()
