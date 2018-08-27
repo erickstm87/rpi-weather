9e8f400197ec2b3baca4f049edfeb01c31e0f23f
Thomas Erickson <terickson@spotx.tv>
2018-08-27 09:55:32 -0600

    have times

--- rpi-weather.py
+++ rpi-weather.py
@@ -1,38 +1,47 @@
 import requests
 from pymongo import MongoClient
 import matplotlib.pyplot as plt
-import datetime
+from datetime import datetime, timedelta
 import pprint
+from pytz import timezone
+import pytz
 
 client = MongoClient('localhost', 27017)
 db = client.pymongo_test
+
+utc = pytz.utc
+
+print('here is the time: ', datetime.now())
 
 r = requests.get('http://api.openweathermap.org/data/2.5/weather?lat=39.7521448&lon=-105.0233895&APPID=977c4567f54065e97d732e98b76e6180&units=imperial')
 response = r.json()
 pprint.pprint(response)
 outside_temp = str(float(response['main']['temp']))
 print('the weather is imperial ', outside_temp)
+hourly = datetime.now().hour
+minutely = datetime.now().minute
+current_time = str(hourly) + ':' + str(minutely)
 
 posts = db.posts
 weather_data = {
     'title': 'Outside Weather',
     'temp': str(outside_temp),
-    'date': datetime.datetime.utcnow()
+    'date': current_time
 }
 result = posts.insert_one(weather_data)
 found_post = posts.find_one({'title': 'Outside Weather'})
-print('Here is the data in db:', found_post)
 
-outside_weather = []
+outside_weather, the_time = [], []
 for post in posts.find():
     outside_weather.append(post['temp'])
-print('here is the collection of temps: ', outside_weather)
-plt.plot(outside_weather)
+    #the_time.append(post['date'])
+
+plt.plot(the_time, outside_weather)
 
 plt.title('Temp In and Out of Place with ' + response['weather'][0]['description'].title() + ' Conditions: ', fontsize=24)
 plt.xlabel('Time', fontsize=14)
 plt.ylabel('Temp Farenheit', fontsize=14)
 
-# plt.show()
+plt.show()
 
 # outside_weather.append()
