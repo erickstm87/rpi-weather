5a06a6da96b4bfe4b0fc7f0a416aba392729365a
Thomas Erickson <terickson@spotx.tv>
2018-08-29 16:57:20 -0600

    svg files

--- api-calls/javascript-calls.py
+++ api-calls/javascript-calls.py
@@ -3,7 +3,7 @@ import pygal
 
 from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
 
-url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+url = 'https://api.github.com/search/repositories?q=language:javascript&sort=stars'
 r = requests.get(url)
 print('Status Code:', r.status_code)
 
@@ -24,7 +24,7 @@ for repo in repo_dicts:
     }
     plot_dicts.append(plot_dict)
 
-my_style = LS('#333366', base_style=LCS)
+my_style = LS('#006600', base_style=LCS)
 my_style.title_font_size = 24
 my_style.label_font_size = 14
 my_style.major_label_font_size = 18
@@ -38,8 +38,8 @@ my_config.width = 1000
 
 chart = pygal.Bar(my_config, style=my_style)
 #chart = pygal.Bar(style=my_style, x_label_rotation=45, show_legend=False)
-chart.title = 'Python'
+chart.title = 'Javascript'
 chart.x_labels = names
 
 chart.add('', plot_dicts)
-chart.render_to_file('/Users/terickson/rpi-weather/api-calls/python_repos.svg')
+chart.render_to_file('/Users/terickson/rpi-weather/api-calls/javascript_repos.svg')
--- copier.sh
+++ copier.sh
@@ -2,3 +2,9 @@
 
 python3 /Users/terickson/rpi-weather/rpi-weather.py
 aws s3 cp weather.svg s3://data-visual/
+python3 ./api-calls/api-calls.py
+aws s3 cp ./api-calls/python_repos.svg s3://data-visual/
+python3 ./api-calls/javascript-calls.py
+aws s3 cp ./api-calls/javascript_repos.svg s3://data-visual/
+python3 ./api-calls/scala-calls.py
+aws s3 cp ./api-calls/scala_repos.svg s3://data-visual/
