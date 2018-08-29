fc8ba890365583f984f8e9b246e1646d57537405
Thomas Erickson <terickson@spotx.tv>
2018-08-29 13:21:23 -0600

    created a bash runner

--- copier.sh
+++ copier.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+python3 rpi-weather.py
 aws s3 cp weather.svg s3://data-visual/
--- uploads/boto.py
+++
@@ -1,8 +0,0 @@
-import boto3
-
-s3 = boto3.resource('s3')
-bucket = s3.Bucket('data-visual')
-#s3.upload_file('weather.svg', 'data-visual', 'weather_one.svg')
-#s3.Bucket('data-visual').upload_file('weather.svg', 'weather-copied.svg')
-with open('weather.svg', 'rb') as data:
-    bucket.upload_fileobj(data, 'weathercopy.svg')
